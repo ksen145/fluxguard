381706388d7a49462ea2353a08708b35656a82bc
Egor Safonov <ksen10405@gmail.com>
2025-08-05 10:11:23 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='fluxguard',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'fluxguard': ['*.py', 'adapters/*.py'],
+    },
+    install_requires=[],
+    author='Egor Safonov',
+    author_email='ksen10405@gmail.com',
+    description='Library for monitoring and adapting asynchronous Python code',
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
